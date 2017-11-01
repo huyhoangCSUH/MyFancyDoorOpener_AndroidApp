968280592d35466741f85720062bd056f954acfa
Huy Hoang <huyhoang@Huys-MacBook-Pro.local>
2017-11-01 17:39:37 -0500

    Minor changes

--- app/src/main/java/io/huyhoang/myfancydooropener/MainActivity.java
+++ app/src/main/java/io/huyhoang/myfancydooropener/MainActivity.java
@@ -22,9 +22,15 @@ import java.io.InputStream;
 import java.io.DataOutputStream;
 import java.net.HttpURLConnection;
 import android.graphics.BitmapFactory;
-
-
+import android.widget.EditText;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import java.lang.Boolean.*;
+import android.util.Log;
+import android.nfc.Tag;
+import android.content.res.*;
+import java.util.*;
+
 
 public class MainActivity extends AppCompatActivity {
     private Context mContext;
@@ -35,6 +41,10 @@ public class MainActivity extends AppCompatActivity {
     private ImageView mImageView;
     private ImageView mImageViewInternal;
     private WebView mWebView;
+
+    //public static final String PREFS_NAME = "API_String";
+    //String apiUrl = Helper.getConfigValue(this, "api_url");
+    //String apiUrl = Helper.getMetaData(this, "api_url");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,7 +53,19 @@ public class MainActivity extends AppCompatActivity {
         mActivity = MainActivity.this;
 
         mWebView = (WebView) findViewById(R.id.webView);
-        mWebView.loadUrl("http://huyhoang.io:8000/video_feed");
+        String apiUrl = Helper.getConfigValue(this, "api_url");
+        mWebView.loadUrl(apiUrl + "/video_feed");
+
+        final Button SetFrate = (Button) findViewById(R.id.setframerate);
+        SetFrate.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                // Create Object and call AsyncTask execute Method
+                final EditText nameField = (EditText) findViewById(R.id.fratebox);
+                String rate = nameField.getText().toString();
+                new SetFrameRate().execute(rate);
+
+            }
+        });
 
         final Button GetInfoData = (Button) findViewById(R.id.getinfo);
         GetInfoData.setOnClickListener(new OnClickListener() {
@@ -65,7 +87,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         final Button ResetButton = (Button) findViewById(R.id.reset);
-        Authorization.setOnClickListener(new OnClickListener() {
+        ResetButton.setOnClickListener(new OnClickListener() {
             public void onClick(View v) {
                 // Create Object and call AsyncTask execute Method
                 new ResetAuth().execute();
@@ -74,19 +96,18 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private class SetAuth  extends AsyncTask<String, Void, Void> {
-        String result;
-        String inputLine;
-
+    private class SetFrameRate extends AsyncTask<String, Void, String> {
         protected void onPreExecute() {
 
         }
 
-        protected Void doInBackground(String... urls) {
+        protected String doInBackground(String... rate) {
 
+            String inputLine;
             try {
                 //Create a URL object holding our url
-                URL url = new URL("http://huyhoang.io:8000/setauth");
+                String apiUrl = Helper.getConfigValue(getApplicationContext(), "api_url");
+                URL url = new URL(apiUrl + "/setframerate?rate=" + rate[0]);
                 //Create a connection
                 HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
                 //Set methods and timeouts
@@ -107,16 +128,16 @@ public class MainActivity extends AppCompatActivity {
                 }
 
                 //Close our InputStream and Buffered reader
-                reader.close();
-                streamReader.close();
+                //reader.close();
+                //streamReader.close();
                 //Set our result equal to our stringBuilder
-                result = stringBuilder.toString();
+                //result = stringBuilder.toString();
             }
             catch(Exception e){
                 e.printStackTrace();
             }
-
-            return null;
+//
+            return "Frame rate set!";
         }
 
         protected void onPostExecute() {
@@ -124,7 +145,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private class ResetAuth  extends AsyncTask<String, Void, Void> {
+    private class SetAuth  extends AsyncTask<String, Void, String> {
         String result;
         String inputLine;
 
@@ -132,11 +153,12 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
-        protected Void doInBackground(String... urls) {
+        protected String doInBackground(String... urls) {
 
             try {
                 //Create a URL object holding our url
-                URL url = new URL("http://huyhoang.io:8000/resetauth");
+                String apiUrl = Helper.getConfigValue(getApplicationContext(), "api_url");
+                URL url = new URL(apiUrl + "/setauth");
                 //Create a connection
                 HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
                 //Set methods and timeouts
@@ -166,11 +188,66 @@ public class MainActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
 
-            return null;
+            return result;
         }
 
-        protected void onPostExecute() {
+        protected void onPostExecute(String returnedVal) {
+            TextView txtView = (TextView) findViewById(R.id.auth_msg);
+            txtView.setTextColor(Color.WHITE);
+            txtView.setText(returnedVal);
+        }
+    }
 
+    private class ResetAuth  extends AsyncTask<String, Void, String> {
+        String result;
+        String inputLine;
+
+        protected void onPreExecute() {
+
+        }
+
+        protected String doInBackground(String... urls) {
+
+            try {
+                //Create a URL object holding our url
+                String apiUrl = Helper.getConfigValue(getApplicationContext(), "api_url");
+                URL url = new URL(apiUrl + "/resetauth");
+                //Create a connection
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                //Set methods and timeouts
+                urlConnection.setRequestMethod(REQUEST_METHOD);
+                urlConnection.setReadTimeout(READ_TIMEOUT);
+                urlConnection.setConnectTimeout(CONNECTION_TIMEOUT);
+
+                urlConnection.connect();
+
+                //Create a new InputStreamReader
+                InputStreamReader streamReader = new InputStreamReader(urlConnection.getInputStream());
+                //Create a new buffered reader and String Builder
+                BufferedReader reader = new BufferedReader(streamReader);
+                StringBuilder stringBuilder = new StringBuilder();
+                //Check if the line we are reading is not null
+                while((inputLine = reader.readLine()) != null){
+                    stringBuilder.append(inputLine);
+                }
+
+                //Close our InputStream and Buffered reader
+                reader.close();
+                streamReader.close();
+                //Set our result equal to our stringBuilder
+                result = stringBuilder.toString();
+            }
+            catch(Exception e){
+                e.printStackTrace();
+            }
+
+            return result;
+        }
+
+        protected void onPostExecute(String returnedVal) {
+            TextView txtView = (TextView) findViewById(R.id.auth_msg);
+            txtView.setTextColor(Color.WHITE);
+            txtView.setText(returnedVal);
         }
     }
 
@@ -187,7 +264,8 @@ public class MainActivity extends AppCompatActivity {
 
             try {
                 //Create a URL object holding our url
-                URL url = new URL("http://huyhoang.io:8000/getname");
+                String apiUrl = Helper.getConfigValue(getApplicationContext(), "api_url");
+                URL url = new URL(apiUrl + "/getname");
                 //Create a connection
                 HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
                 //Set methods and timeouts
@@ -240,7 +318,8 @@ public class MainActivity extends AppCompatActivity {
 
             try {
                 //Create a URL object holding our url
-                URL url = new URL("http://huyhoang.io:8000/getdistance");
+                String apiUrl = Helper.getConfigValue(getApplicationContext(), "api_url");
+                URL url = new URL(apiUrl + "/getdistance");
                 //Create a connection
                 HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
                 //Set methods and timeouts
@@ -280,6 +359,8 @@ public class MainActivity extends AppCompatActivity {
             txtView.setText(returnedVal);
         }
     }
+
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
